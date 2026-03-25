71bedc18a90cd4abcda7081b8a13b5356e488dc9
Adam Secada <amsecada@users.noreply.github.com>
2026-03-25 18:11:58 -0500

    Remove Solutions page and nav link

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,5 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
-import Solutions from './pages/Solutions';
 import CaseStudies from './pages/CaseStudies';
 import Resources from './pages/Resources';
 import BookCall from './pages/BookCall';
@@ -12,7 +11,6 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
-        <Route path="/solutions" element={<Solutions />} />
         <Route path="/case-studies" element={<CaseStudies />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/book-call" element={<BookCall />} />
