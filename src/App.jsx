0b752ac35f75fc7f148d5cbfe375e2ed4f6b1bf7
Adam Secada <amsecada@users.noreply.github.com>
2026-03-27 13:29:46 -0500

    Add case study template page and link case study cards

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import CaseStudies from './pages/CaseStudies';
+import CaseStudyTemplate from './pages/CaseStudyTemplate';
 import Resources from './pages/Resources';
 import BookCall from './pages/BookCall';
 import Products from './pages/Products';
@@ -12,6 +13,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/products" element={<Products />} />
         <Route path="/case-studies" element={<CaseStudies />} />
+        <Route path="/case-studies/template-atlas-home-services" element={<CaseStudyTemplate />} />
         <Route path="/resources" element={<Resources />} />
         <Route path="/book-call" element={<BookCall />} />
       </Routes>
